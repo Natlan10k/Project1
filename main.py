d555774c18682336d057ffc6da7c63554248c1d0
Natlan10k <knavesseries@gmail.com>
2024-06-08 02:14:30 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,41 @@
+# Creating a dictionary
+todo_list = {}
+
+# Adding items to the dictionary
+todo_list['task1'] = 'Complete Python tutorial'
+todo_list['task2'] = 'Read a book on data structures'
+todo_list['task3'] = '4'
+# You can also store integers as values in a dictionary
+todo_list['task4'] = 4
+todo_list['task5'] = 5
+
+# Cast the data type
+character1 = 6
+print(type(character1))
+character1 = str(character1)
+print(type(character1))
+
+
+
+# # Printing the dictionary
+# print("Initial To-Do List:")
+# for task, description in todo_list.items():
+#     print(f"{task}: {description}")
+
+# # Retrieving an item by key
+# print("\nDescription for task1:")
+# print(todo_list['task1'])
+
+# # Updating an item
+# todo_list['task1'] = 'Complete Python tutorial and practice exercises'
+
+# # Adding a new item
+# todo_list['task3'] = 'Go for a run'
+
+# # Removing an item
+# del todo_list['task2']
+
+# # Printing the updated dictionary
+# print("\nUpdated To-Do List:")
+# for task, description in todo_list.items():
+#     print(f"{task}: {description}")
